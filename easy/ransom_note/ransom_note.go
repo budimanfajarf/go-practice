e801ef4581909a9b6a58489ed5513143c681dee2
Budi☕ <budimanfajarf@gmail.com>
2025-11-03 18:17:12 +0700

    refactor(easy): ransom note approach

--- easy/ransom_note/ransom_note.go
+++ easy/ransom_note/ransom_note.go
@@ -1,19 +1,40 @@
 package ransom_note
 
 func CanConstruct(ransomNote string, magazine string) bool {
-	ransomNoteLength := len(ransomNote)
-	ransomNoteCount := 0
-	usedMagazine := make(map[int]bool)
+	// Approach 1: Using map to track used characters in magazine
+	// ransomNoteLength := len(ransomNote)
+	// ransomNoteCount := 0
+	// usedMagazine := make(map[int]bool)
 
-	for i := 0; i < ransomNoteLength; i++ {
-		for j := 0; j < len(magazine); j++ {
-			if ransomNote[i] == magazine[j] && !usedMagazine[j] {
-				ransomNoteCount++
-				usedMagazine[j] = true
-				break
-			}
-		}
+	// for i := 0; i < ransomNoteLength; i++ {
+	// 	for j := 0; j < len(magazine); j++ {
+	// 		if ransomNote[i] == magazine[j] && !usedMagazine[j] {
+	// 			ransomNoteCount++
+	// 			usedMagazine[j] = true
+	// 			break
+	// 		}
+	// 	}
+	// }
+
+	// return ransomNoteCount == ransomNoteLength
+	// time complexity: O(n*m)
+	// space complexity: O(m)
+
+	// Approach 2: Using character count map
+	charCount := make(map[rune]int)
+
+	for _, char := range magazine {
+		charCount[char]++
 	}
 
-	return ransomNoteCount == ransomNoteLength
+	for _, char := range ransomNote {
+		if charCount[char] == 0 {
+			return false
+		}
+		charCount[char]--
+	}
+
+	return true
+	// time complexity: O(m)
+	// space complexity: O(k)
 }
