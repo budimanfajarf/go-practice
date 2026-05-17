a9f9f1c8786ae6a076be2a394c88d6797898eeca
Budi☕ <budimanfajarf@gmail.com>
2026-05-18 01:28:13 +0700

    refactor: add comment to top k

--- medium/top_k/top_k.go
+++ medium/top_k/top_k.go
@@ -4,18 +4,22 @@ func TopKFrequent(nums []int, k int) []int {
 	numFrequents := map[int]int{}
 	frequentNums := map[int][]int{}
 
+	// Count the frequency of each number.
 	for _, num := range nums {
 		numFrequents[num]++
 	}
 
+	// Group numbers by their frequency.
 	for num, freq := range numFrequents {
 		frequentNums[freq] = append(frequentNums[freq], num)
 	}
 
+	// Iterate through the possible top frequencies from the maximum of "nums" in descending order,
+	// and collect the top frequent numbers until we have "k" of them.
 	topK := []int{}
 	for i := len(nums); i > 0; i-- {
-		numsInFrequent, ok := frequentNums[i]
-		if !ok {
+		numsInFrequent, exist := frequentNums[i]
+		if !exist {
 			continue
 		}
 
